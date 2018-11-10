3485456689ad7be7767c9ba87e3671bd90524a9f
Mateusz Archiciński <mateusz.archicinski@decerto.pl>
2018-11-10 23:33:59 +0100

    perf(app): removing unused files

--- src/app/components/bank/bank-account/bank-account.component.ts
+++ src/app/components/bank/bank-account/bank-account.component.ts
@@ -1,20 +1,26 @@
+import { Component, Input } from '@angular/core';
 
-import { Component, Input, OnInit } from "@angular/core";
 import { BankAccount } from '../../../interfaces/resources/bank/bank-account';
-import { ApiService } from "../../../services/api/api.service";
+import { ApiService } from '../../../services/api/api.service';
 
 @Component({
-  selector: 'app-bank-account',
-  templateUrl: './bank-account.component.html',
-  styleUrls: ['./bank-account.component.scss']
+    selector: 'app-bank-account',
+    templateUrl: './bank-account.component.html',
+    styleUrls: ['./bank-account.component.scss']
 })
-export class BankAccountComponent implements OnInit {
+export class BankAccountComponent {
 
-  @Input() bankAccount: BankAccount;
+    @Input() set bankAccount(v: BankAccount) {
+        this._bankAccount = v
+    }
 
-  constructor(private readonly api: ApiService) { }
+    get bankAccount(): BankAccount {
+        return this._bankAccount || this.api.getAccount();
+    }
 
-  ngOnInit() {
-    this.bankAccount = this.api.getAccount();
-  }
+    private _bankAccount: BankAccount;
+
+    constructor(private api: ApiService) {
+    }
+
 }
